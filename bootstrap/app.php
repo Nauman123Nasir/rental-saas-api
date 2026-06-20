d52ab486a17b498ddeb0e7972e198df6de49156d
Nauman Nasir <nauman.nasir@iskaan.com>
2026-06-20 19:42:32 +0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'permission' => \App\Http\Middleware\CheckPermission::class,
+            'check.permission' => \App\Http\Middleware\CheckPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
